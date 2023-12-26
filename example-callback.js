5b1ba9ac928796758a2156ea93c46930506a830f
mertcetiin <mrtctn506@gmail.com>
2023-12-26 15:22:29 +0300

    request loaded

---
+++ example-callback.js
@@ -0,0 +1,6 @@
+const request = require('request');
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=istanbul,tr&appid=33fb53775a5f8109e23b6e58a77223e3&units=metric';
+
+request({
+
+})
