68fe4a91122984d67cc534f6906cfaa27f2d790a
mertcetiin <mrtctn506@gmail.com>
2023-12-27 22:33:10 +0300

    location.js promise made

--- location.js
+++ location.js
@@ -1,15 +1,17 @@
 const request = require('request');
 const url = 'http://ipinfo.io';
 
-module.exports = function (callback) {
-    request({
-        url: url,
-        json: true
-    }, function (error, response, body) {
-        if (error) {
-            callback();
-        } else {
-            callback(body);
-        }
-    });
+module.exports = function () {
+    return new Promise((resolve, reject) => {
+        request({
+            url: url,
+            json: true
+        }, function (error, response, body) {
+            if (error) {
+                reject();
+            } else {
+                resolve(body);
+            }
+        });
+    })
 }
