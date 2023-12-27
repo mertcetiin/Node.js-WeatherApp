5df77c9a6bfaed1319537a4b24a8b541cf24bb2e
mertcetiin <mrtctn506@gmail.com>
2023-12-27 22:31:23 +0300

    weather.js promise made

--- weather.js
+++ weather.js
@@ -1,24 +1,27 @@
 const request = require('request');
 
 
-module.exports = function (location, callback) {
+module.exports = function (location) {
 
-    const encodedLocation = encodeURIComponent(location);
+    return new Promise(function (resolve, reject) {
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + encodedLocation + ",tr&appid=33fb53775a5f8109e23b6e58a77223e3&units=metric";
+        const encodedLocation = encodeURIComponent(location);
 
-    if (!location) {
-        return callback('Lokasyon bilgisi alinamadi!!');
-    }
+        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + encodedLocation + ",tr&appid=33fb53775a5f8109e23b6e58a77223e3&units=metric";
 
-    request({
-        url: url,
-        json: true
-    }, function (error, response, body) {
-        if (error) {
-            callback('Hava durumu alinamadi!!');
-        } else {
-            callback(`${body.name}'da hava sicakligi: ${body.main.temp}`)
+        if (!location) {
+            return reject('Lokasyon bilgisi alinamadi!!');
         }
-    });
+
+        request({
+            url: url,
+            json: true
+        }, function (error, response, body) {
+            if (error) {
+                reject('Hava durumu alinamadi!!');
+            } else {
+                resolve(`${body.name}'da hava sicakligi: ${body.main.temp}`)
+            }
+        });
+    })
 }
